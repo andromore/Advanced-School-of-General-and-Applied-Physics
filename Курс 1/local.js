8e7a12c4a531d42ffc790f112e39e52b4a01c425
andromore <andromore@yandex.ru>
2024-06-10 21:39:04 +0300

    Добавлены цитаты и учебная практика

--- Курс 1/local.js
+++ Курс 1/local.js
@@ -6,9 +6,10 @@ menu = [{innerText: "Аналитическая геометрия", href: "./К
 { innerText: "История", href: "./Курс 1/История/index.html" }]
 site.add_navigation("Семестр 1", menu);
 menu = [{ innerText: "Английский язык", href: "./Английский язык/index.html" },
+{ innerText: "История", href: "./Курс 1/История/index.html" },
 { innerText: "Линейная алгебра", href: "./Курс 1/Линейная алгебра/index.html" },
 { innerText: "Математический анализ", href: "./Курс 1/Математический анализ/index.html" },
 { innerText: "Общий физический практикум", href: "./Общий физический практикум/index.html" },
 { innerText: "Термодинамика и молекулярная физика", href: "./Курс 1/Термодинамика и молекулярная физика/index.html" },
-{ innerText: "История", href: "./Курс 1/История/index.html" }]
+{ innerText: "Учебная практика", href: "./Учебная практика/index.html"}]
 site.add_navigation("Семестр 2", menu);
