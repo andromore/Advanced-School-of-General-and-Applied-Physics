74aef8443450284a5bb3378971fdd21b031cec3d
andromore <andromore@yandex.ru>
2024-06-25 20:19:31 +0300

    Некоторые поделки и недоделанный алгоритм парсинга

--- lib.py
+++ lib.py
@@ -6,6 +6,9 @@ def __passing(*a, **b):
     """Do nothing"""
     pass
 
+def message(message: str):
+    return f"\n--- {message} ---\n"
+
 def partial(function, *a, **b):
     """Get function with substituted arguments"""
     def func(*c, **d):
@@ -14,60 +17,83 @@ def partial(function, *a, **b):
 
 def filepath(filename: str, dirs: list):
     """Get full path to file [filename] in place dirs[0]/dirs[1]/dirs[2]/..."""
+    assert type(filepath) == str and type(dirs) == list
     return "/".join(dirs) + "/" + filename
 
 class File:
     """Class of file"""
     def __init__(self, filepath: str):
         assert type(filepath) == str, ValueError("\n--- Path of file must be <str> ---\n")
-        self.filename = filepath
-        self.text = None
+        self._filename = filepath
+        self._text = None
 
     def extension(self):
         """Get file extension"""
-        if "." not in self.filename:
+        if "." not in self._filename:
             return None
-        tmp = self.filename.split(".")
+        tmp = self._filename.split(".")
         if tmp[-1][0] == '/':
             return None
         if '.' in tmp[-1]:
             raise Exception("\n--- Bad filepath ---\n")
         return tmp[-1]
     
-    def open(self):
-        if self.text:
-            raise Exception(f"\n--- File {self.filename} is already opened ---\n")
-        else:
-            try:
-                with open(self.filename, mode = 'r') as file:
-                    self.text = file.read()
-            except Exception as exception:
-                print(f"\n--- While opening file {self.filename} I've got an error: {exception} ---\n")
+    @property
+    def text(self) -> str:
+        return self._text
+    
+    @text.setter
+    def text(self, text: str) -> None:
+        assert type(text) == str, ValueError("\n--- Text should be <str> ---\n")
+        self._text = text
+        
+    @property
+    def name(self) -> str:
+        return self._filename
+    
+    @name.setter
+    def name(self, name: str):
+        assert type(name) == str, ValueError(message("Name should be <str>"))
+        if self._text:
+            raise Exception(message("File isn't closed"))
+        self._filename = name
+        
+    @classmethod
+    def open(cls, filename: str):
+        assert type(filename) == str, ValueError(message("Filename should be <str>"))
+        try:
+            with open(filename, mode = 'r') as file:
+                text = file.read()
+            result = cls(filename)
+            result.text = text
+            return result
+        except Exception as exception:
+            raise Exception(f"\n--- While opening file {filename} I've got an error: {exception} ---\n")
     
     def close(self, save: bool = True):
-        if not self.text:
-            raise Exception(f"\n--- There is no file to close or file {self.filename} is already closed ---\n")
+        if not self._text:
+            raise Exception(f"\n--- There is no file to close or file {self._filename} is already closed ---\n")
         else:
             if save:
                 self.save()
-            self.text = None
+            self._text = None
     
     def save(self):
-        if not self.text:
-            raise Exception(f"\n--- There is nothing to save in {self.filename} ---\n")
+        if not self._text:
+            raise Exception(f"\n--- There is nothing to save in {self._filename} ---\n")
         else:
-            with open(self.filename, mode = 'w') as file:
-                file.write(self.text)
+            with open(self._filename, mode = 'w') as file:
+                file.write(self._text)
     
     def replace(self, old: str, new: str = ""):
-        if not self.text:
+        if not self._text:
             raise Exception(f"\n--- There is no file to replace ---\n")
         else:
-            self.text = self.text.replace(old, new)
+            self._text = self._text.replace(old, new)
             
     def write(self, text: str):
         assert type(text) == str
-        self.text = text
+        self._text = text
 
 """Function handle() for recursive deals with files"""
 
@@ -96,12 +122,61 @@ def printer(filepath: str):
 
 def replacer(filename, old, new):
     """Replace all [old] to [new]"""
-    file = File(filename)
-    file.open()
+    file = File.open(filename)
     file.replace(old, new)
     file.close()
 
 """ XML\HTML parsing """
+
+class Declaration:
+    """XML\HTML Declaration"""
+    
+    def __init__(self, begin, end, **kwargs: str | list[str]):
+        """kwargs key "_class" will be turned to "class" """
+        assert type(begin) == str and type(end) == str, ValueError(message("Beginning and ending must be <str>"))
+        for i in kwargs.keys():
+            assert " " not in i, KeyError("\n--- Key mustn't contain spaces ---\n")
+            assert type(kwargs[i]) in (list, str), ValueError("\n--- Values must be <str> or <list> of <str> ---\n")
+            if type(kwargs[i]) == list:
+                for i in kwargs[i]:
+                    assert type(kwargs[i]) == str, ValueError("\n--- Values must be <str> or <list> of <str> ---\n")
+        self._attributes = kwargs
+        self._begin = begin
+        self._end = end
+    
+    def __str__(self):
+        result = self._begin
+        for key in self._attributes.keys():
+            if not self._attributes[key]:
+                result += f" {key}"
+                continue
+            result += f" {key}=\"{self._attributes[key] if type(self._attributes[key]) == str else ' '.join(self._attributes[key])}\""
+        result += f" {self._end}"
+        return f"<{result}>"
+        
+    def __getitem__(self, key):
+        assert type(key) == str, ValueError("\n--- Type of [key] must be <str> ---\n")
+        for i in self._attributes.keys():
+            assert " " not in i, KeyError("\n--- Key mustn't contain spaces ---\n")
+        if key not in self._attributes.keys():
+            return None
+        return self._attributes[key]
+    
+    def __setitem__(self, key, value):
+        assert type(key) == str or type(value) in (str, list), ValueError("\n--- Type of [key] and [value] must be <str> or <list[str]> ---\n")
+        if type(value) == list:
+            for i in value:
+                assert type(i) == str, ValueError("\n--- Type of [key] and [value] must be <str> or <list[str]> ---\n")
+        for i in self._attributes.keys():
+            assert " " not in i, KeyError("\n--- Key mustn't contain spaces ---\n")
+        self._attributes[key] = value
+        
+    def __delitem__(self, key):
+        assert type(key) == str, ValueError("\n--- Type of [key] must be <str> ---\n")
+        for i in self._attributes.keys():
+            assert " " not in i, KeyError("\n--- Key mustn't contain spaces ---\n")
+        assert key in self._attributes.keys(), KeyError("\n--- It is very difficult to kill a dead ---\n")
+        del self._attributes[key]
 
 class Comment:
     """XML\HTML Comment"""
@@ -143,6 +218,9 @@ class Tag:
     def __str__(self):
         result = self._tag
         for key in self._attributes.keys():
+            if not self._attributes[key]:
+                result += f" {key}"
+                continue
             result += f" {key}=\"{self._attributes[key] if type(self._attributes[key]) == str else ' '.join(self._attributes[key])}\""
         if self._children:
             children = ""
@@ -191,3 +269,133 @@ class Tag:
         """Configure tag"""
         assert type(value) == str, ValueError("\n--- Tag must be <str> ---\n")
         self._tag = value
+        
+def parse(text: str):
+    assert type(text) == str
+    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    
+    OPENING = "opening"
+    CLOSING = "closing"
+    CLOSED = "closed"
+    
+    def kwargs(text: str):
+        tag = ""
+        attributes = dict()
+        cursor = 0
+        eof = len(text)
+        while text[cursor] != ' ' and cursor + 1 < eof:
+            tag += text[cursor]
+            cursor += 1
+        if cursor + 1 >= eof:
+            return text, attributes
+        cursor += 1
+        while cursor + 1 < eof:
+            key = ""
+            value = ""
+            while text[cursor] not in '= ' and cursor + 1 < eof:
+                key += text[cursor]
+                cursor += 1
+            if text[cursor] == ' ':
+                cursor += 1
+                attributes.update({key: ""})
+                continue
+            cursor += 1
+            if text[cursor] == '"':
+                cursor += 1
+                while text[cursor] != '"' and cursor + 1 < eof:
+                    value += text[cursor]
+                    cursor += 1
+                cursor += 1
+                attributes.update({key: value})
+                continue
+            while text[cursor] != ' ' and cursor + 1 < eof:
+                value += text[cursor]
+                cursor += 1
+            cursor += 1
+            attributes.update({key: value})
+            continue
+        if '' in attributes.keys():
+            del attributes['']
+        return text, tag, attributes
+            
+    class ParseTag:
+        def __init__(self, text: str):
+            text = text.replace('\n', ' ').replace("  ", ' ')
+            self.tag = ""
+            self.attributes = dict()
+            self.type = ""
+            if '/' not in text:
+                self.type = OPENING
+            elif '/' == text[1]:
+                self.type = CLOSING
+            elif '/' == text[-2]:
+                self.type = CLOSED
+                self.tag = text[1:-1]
+                return
+            else:
+                raise Exception(message("While parsing was haven't nonetype tag"))
+            
+        @property
+        def closing(self) -> str:
+            if self.type in (CLOSED, CLOSING):
+                return None
+            elif self.type == OPENING:
+                return ParseTag(f"</{self.tag}>")
+            else:
+                raise            
+            
+        def __str__(self):
+            return ""
+    
+    result = []
+    cursor = 0
+    results = 0
+    eof = len(text)
+    
+    while cursor + 1 < eof:
+        results += 1
+        result.append('')
+        while text[cursor] != '<' and cursor + 1 < eof:
+            result[results - 1] += text[cursor]
+            cursor += 1
+        results += 1
+        cursor += 1
+        result.append('<')
+        if text[cursor:cursor+3] == "!--":
+            cursor += 3
+            result[results - 1] += "!--"
+            while text[cursor:cursor+3] != "-->" and cursor + 1 < eof:
+                result[results - 1] += text[cursor]
+                cursor += 1
+            cursor += 3
+            result[results - 1] += "-->"
+            continue
+        while text[cursor] != '>' and cursor + 1 < eof:
+            if text[cursor] == '\"':
+                result[results - 1] += text[cursor]
+                cursor += 1
+                while text[cursor] != "\"" and cursor + 1 < eof:
+                    result[results - 1] += text[cursor]
+                    cursor += 1
+            result[results - 1] += text[cursor]
+            cursor += 1
+        cursor += 1
+        result[results - 1] += '>'
+        
+    for i in range(len(result)):
+        if len(result[i]) >= 7 and '<!--' == result[i][:4] and '-->' == result[i][-3:]: # Comment
+            result[i] = Comment(result[i][4:-3])
+        elif len(result[i]) >= 3 and '<' == result[i][0] and '>' == result[i][-1] and result[i][1] not in (alphabet + '/'): # Declaration
+            ...
+        elif len(result[i]) >= 3 and '<' == result[i][0] and '>' == result[i][-1]: # Tag
+            ...
+        elif '<' not in result[i] and '>' not in result[i]: # Text
+            pass
+        else:
+            raise Exception(message("While scanning file an error has occurred"))
+        
+    return result
+    
+    
+file = File.open("./index.html")    
+parse(file.text)
